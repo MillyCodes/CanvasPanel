d46e049f40020a5b7c5dc67d3fdda7a756525c8b
Milly R <millz118@gmail.com>
2018-09-17 11:31:58 -0400

    updated delete feature

--- app/models/cohort.rb
+++ app/models/cohort.rb
@@ -2,7 +2,7 @@ class Cohort < ApplicationRecord
     has_many :admins
     belongs_to :course
     belongs_to :instructor
-    has_many :students
+    has_many :students, dependent: :destroy
 end
 
 #can model have multiple belong_to statements???
--- app/models/instructor.rb
+++ app/models/instructor.rb
@@ -1,6 +1,6 @@
 class Instructor < ApplicationRecord
     has_many :admins
-    has_many :cohorts
+    has_many :cohorts, dependent: :destroy
     has_one_attached :avatar
 
     def full_name 
